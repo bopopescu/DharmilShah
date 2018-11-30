7c44fa657e637b0abc7e805b50111721e80cca99
unknown <dharmil@jbspl.com>
2018-11-30 18:59:59 +0530

    30-11-18

--- Solidity_task/privatenetworkcodes/rinkeby/startnode.cmd
+++
@@ -1,1 +0,0 @@
-geth --networkid 4 --mine --minerthreads 1 --rinkeby --datadir "." --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password ./password.sec
